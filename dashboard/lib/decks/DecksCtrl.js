aa1ac7b6c7906399207a23c1dd51dfbb6b73eb33
Julien Colin <juliencolin54@gmail.com>
2016-06-07 16:30:49 +0100

    Update header index.html

--- dashboard/lib/decks/DecksCtrl.js
+++ dashboard/lib/decks/DecksCtrl.js
@@ -4,7 +4,7 @@
 
     var my_decks = require("data/my_decks");
 
-    angular.module('app').controller('DecksCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    angular.module('app').controller('DecksCtrl', ['$scope', function ($scope) {
 
         $scope.decks = my_decks.getAll();
 
