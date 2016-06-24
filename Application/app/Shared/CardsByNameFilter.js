785e65abd123e56c2dc0ea60106086d145a6f290
Maigret Aurelien <dewep.net@gmail.com>
2016-06-24 14:53:14 +0100

    Fix Filter

--- Application/app/Shared/CardsByNameFilter.js
+++ Application/app/Shared/CardsByNameFilter.js
@@ -1,6 +1,6 @@
 (function () {
 
-    angular.module('app').filter('hsData', ['cardsByName', function (CardsService) {
+    angular.module('app').filter('cardsByName', ['CardsService', function (CardsService) {
         return function(cardName) {
             var result = CardsService.searchCardOfName(cardName);
             return result === null ? '' : result.name;
