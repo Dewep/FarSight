10f6fcb993056fcea4d895e93df7ce4b9d3ab2c1
Julien Colin <juliencolin54@gmail.com>
2016-08-21 17:02:07 +0200

    update code with comment

--- dashboard/lib/app.js
+++ dashboard/lib/app.js
@@ -2,43 +2,19 @@
 
     'use strict';
 
+    // Loads Angular lib
     var angular = require("angular");
 
-    angular.module('app.directive', []);
-
-    angular.module('app.service', []);
-
-    angular.module('app.filter', []);
-
-    angular.module('app.templates', []);
-
-    angular.module('app', [
-        require('angular-route'),
-
-        'app.directive',
-        'app.service',
-        'app.filter',
-        'app.templates'
-    ]).config(['$routeProvider', function($routeProvider) {
-        $routeProvider/*.when('/', {
-            templateUrl: 'lib/home/home.html',
-            controller: 'HomeCtrl'
-        }).when('/my_decks/', {
-            templateUrl: 'lib/my_decks/list.html',
-            controller: 'MydecksCtrl'
-        }).when('/my_decks/new/', {
-            templateUrl: 'lib/my_decks/new.html',
-            controller: 'MydecksCtrl'
-        }).when('/my_decks/:id/', {
-            templateUrl: 'lib/my_decks/detail.ht ml',
-            controller: 'MydecksCtrl'
-        })*/.when('/in_game/', {
-            templateUrl: 'lib/ingame/ingame.html',
-            controller: 'IngameCtrl'
-        }).otherwise({
-            //redirectTo: '/'
-            redirectTo: '/in_game/'
-        });
-    }]);
-
+    // Creates the app module and defines the routes
+    angular
+        .module('app', [require('angular-route')])
+        .config(['$routeProvider', function($routeProvider) {
+            // /in_game/ URL loads ingame.html with its controller IngameCtrl
+            $routeProvider.when('/in_game/', {
+                templateUrl: 'lib/ingame/ingame.html',
+                controller: 'IngameCtrl'
+            }).otherwise({  // By default goes to /in_game/
+                redirectTo: '/in_game/'
+            });
+        }]);
 })();
--- dashboard/lib/ingame/ingame.controller.js
+++ dashboard/lib/ingame/ingame.controller.js
@@ -2,22 +2,12 @@
 
     'use strict';
 
-    var watcher = require("log/watcher");
-
-    angular.module('app').controller('IngameCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
-
-        /*window.refresh_ingame = function refresh_ingame(new_value) {
-            $timeout(function () {
-                $scope.player_cards_hand_size = new_value;
-            });
-        };*/
-
+    angular.module('app').controller('IngameCtrl', function ($scope) {
+        // $scope contains the data accesible from the view
         $scope.player_cards_hand_size = 3;
         $scope.player_cards_deck_size = 6;
-
         $scope.opponent_cards_hand_size = 4;
         $scope.opponent_cards_deck_size = 19;
-
         $scope.opponent_deck_prediction = [
             {"deck": "Zoo (Warlock)", "percent": 0.4},
             {"deck": "Zoo 2 (Warlock)", "percent": 0.3},
@@ -25,42 +15,6 @@
             {"deck": "Reno (Warlock)", "percent": 0.05}
         ];
 
-        $scope.player_advices = [
-            {"message": "Do that."},
-            {"message": "Next that."},
-            {"message": "And that."}
-        ];
-
-        $scope.opponent_plays_prediction = [
-            {"card_id": "EX1_560", "percent": 0.4},
-            {"card_id": "EX1_561", "percent": 0.2},
-            {"card_id": "EX1_562", "percent": 0.2}
-        ];
-
-        $scope.player_cards_deck = [
-            {"card_id": "EX1_563", "number": 1},
-            {"card_id": "EX1_564", "number": 2},
-            {"card_id": "EX1_565", "number": 2},
-            {"card_id": "EX1_567", "number": 1}
-        ];
-
-        /*$scope.player_cards_hand = [
-            {"card_id": "EX1_560", "number": 1},
-            {"card_id": "EX1_561", "number": 2}
-        ];*/
-
-        /*$scope.player_deck_prediction = [
-            {"deck": "Zoo (Warlock)", "percent": 0.9},
-            {"deck": "Basic (Warlock)", "percent": 0.1}
-        ];*/
-
-        watcher.Handler(function(data) {
-            $timeout(function () {
-                console.log("GameWatcherData", data);
-                //$scope.player_cards_hand_size = new_value;
-            });
-        });
-
-    }]);
+    });
 
 })();
