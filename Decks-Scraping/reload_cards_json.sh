56a7857d0dfa7c344a5050c41f2444f1373e14d1
Julien Colin <juliencolin54@gmail.com>
2016-08-26 16:31:40 +0200

    Follow redirection

--- Decks-Scraping/reload_cards_json.sh
+++ Decks-Scraping/reload_cards_json.sh
@@ -4,4 +4,4 @@
 #!/bin/bash
 
 # Update cards list
-curl https://api.hearthstonejson.com/v1/latest/enUS/cards.json > cards.json
+curl -L https://api.hearthstonejson.com/v1/latest/enUS/cards.json > cards.json
